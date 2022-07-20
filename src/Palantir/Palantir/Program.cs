0b4d5a73cd33c71e2ee785d4eec8cc4458f05310
Matthias Hillermeier <m.hillermeier@adestis.de>
2022-07-20 20:21:36 +0200

    proto actor

---
+++ src/Palantir/Palantir/Program.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.OpenApi;
+using Proto;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+var system = new ActorSystem();
+var props = Props.FromProducer(() => new GreetingActor());
+var greeter = system.Root.Spawn(props);
+
+// Send a message to the actor
+system.Root.Send(greeter, new Greet("World"));
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
+public class Greet
+{
+    public Greet(string who)
+    {
+        Who = who;
+    }
+    public string Who { get; private set; }
+}
+
+// Create the actor class
+public class GreetingActor : IActor
+{
+    public Task ReceiveAsync(IContext ctx)
+    {
+        if (ctx.Message is Greet greet)
+        {
+            // Tell the actor to respond
+            // to the Greet message
+            Console.WriteLine($"Hello {greet.Who}");
+        }
+        return Task.CompletedTask;
+    }
+}
+
