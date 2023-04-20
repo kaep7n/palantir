248b2ee52d11acd8e4ebe0cb67c9f3947ec51dad
Matthias Hillermeier <m.hillermeier@adestis.de>
2023-04-20 12:53:02 +0200

    feat(base): pub sub

--- src/Palantir.Homatic/Actors/HomaticDeviceChannelActor.cs
+++ src/Palantir.Homatic/Actors/HomaticDeviceChannelActor.cs
@@ -2,6 +2,7 @@
 using Palantir.Homatic.Http;
 using Proto;
 using Proto.Cluster;
+using Proto.Cluster.PubSub;
 using Proto.DependencyInjection;
 
 namespace Palantir.Homatic.Actors;
@@ -31,38 +32,31 @@ public class HomaticDeviceChannelActor : IActor
     {
         if (context.Message is Started)
         {
-            try
+            this.logger.LogDebug("{type} ({pid}) has started", this.GetType(), context.Self);
+
+            var channel = await this.homaticClient.GetChannelAsync(this.deviceId, this.id);
+
+            var homaticRooms = channel.Links.Where(l => l.Rel == "room");
+
+            foreach (var homaticRoom in homaticRooms)
             {
-                this.logger.LogDebug("{type} ({pid}) has started", this.GetType(), context.Self);
+                var homaticRoomId = homaticRoom.Href.Replace("/room/", string.Empty);
 
-                var channel = await this.homaticClient.GetChannelAsync(this.deviceId, this.id);
+                var room = context.Cluster().GetRoomGrain(Rooms.GetClusterIdentity(homaticRoomId));
 
-                var homaticRooms = channel.Links.Where(l => l.Rel == "room");
-
-                foreach (var homaticRoom in homaticRooms)
-                {
-                    var homaticRoomId = homaticRoom.Href.Replace("/room/", string.Empty);
-
-                    var room = context.Cluster().GetRoomGrain(Rooms.GetClusterIdentity(homaticRoomId));
-
-                    var joinRoom = new JoinRoom(this.deviceId, this.id, homaticRoom.Href.Replace("/room/", string.Empty));
-                    var roomJoined = await room.Join(joinRoom, CancellationToken.None);
-                }
-
-                foreach (var link in channel.Links)
-                {
-                    if (link.Href == ".." || link.Rel != "parameter")
-                        continue;
-
-                    var props = context.System.DI().PropsFor<HomaticDeviceChannelParameterActor>(this.deviceId, this.id, link.Href);
-                    var pid = context.Spawn(props);
-
-                    this.parameters.Add(link.Href, pid);
-                }
+                var joinRoom = new JoinRoom(this.deviceId, this.id, homaticRoom.Href.Replace("/room/", string.Empty));
+                var roomJoined = await room.Join(joinRoom, CancellationToken.None);
             }
-            catch (Exception exception)
+
+            foreach (var link in channel.Links)
             {
-                this.logger.LogError(exception, "HomaticDeviceChannelActor");
+                if (link.Href == ".." || link.Rel != "parameter")
+                    continue;
+
+                var props = context.System.DI().PropsFor<HomaticDeviceChannelParameterActor>(this.deviceId, this.id, link.Href);
+                var pid = context.Spawn(props);
+
+                this.parameters.Add(link.Href, pid);
             }
         }
         if (context.Message is ParameterValueChanged pvc)
@@ -70,6 +64,10 @@ public class HomaticDeviceChannelActor : IActor
             var parameter = this.parameters[pvc.Parameter];
 
             context.Forward(parameter);
+
+            var publisher = context.Cluster().Publisher();
+
+            await publisher.Publish($"values/{pvc.Device}/{pvc.Channel}", new ValueChanged { Type = "test", Value = pvc.Value.ToString() });
         }
         if (context.Message is Stopped)
         {
--- src/Palantir/Room.cs
+++ src/Palantir/Room.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Proto;
 using Proto.Cluster;
+using Proto.Cluster.PubSub;
 
 namespace Palantir;
 
@@ -30,6 +31,17 @@ public class Room : RoomGrainBase
     public override Task<RoomJoined> Join(JoinRoom request)
     {
         this.logger.LogInformation($"device {request.DeviceId} {request.ChannelId} joined Room {this.clusterIdentity}");
+
+        this.Cluster.Subscribe($"values/{request.DeviceId}/{request.ChannelId}", context =>
+        {
+            if (context.Message is ValueChanged vc)
+            {
+                this.logger.LogInformation($"{this.clusterIdentity}: value changed {vc}");
+            }
+
+            return Task.CompletedTask;
+        });
+
         return Task.FromResult(new RoomJoined());
     }
 
